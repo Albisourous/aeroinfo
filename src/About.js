dbecd4e5869622e828a37b3ef3fc8a7abc66794e
Albin Shrestha <shrestha_albin@yahoo.com>
2020-06-24 00:20:13 -0500

    added a better about page with an example of myself with a css to fomat it. Also added some tester files in temp and update which can be used to run tests on code

--- src/About.js
+++ src/About.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import albin from './Images/albin.jpg'
+import './about.css'
 
+console.log(albin);
 export const About = () => (
-    <div>
-        <h2>About page</h2>
+    <div className="about">
+        <div><br/></div>
+        <div className="row">
+            <div className="member">
+                <div><br/></div>
+                <h2>Albin Shrestha</h2>
+                <div class="pic">
+                    <img src={albin}/>
+                </div>
+                <p className="role">Frontend</p>
+                <p>Albin Shrestha connects tech and project management companies with the information and resources they need in order to properly train their employees. At the University of Texas, Albin has learned how to appropriately communicate with peers and co-workers creating a productive workflow. His personal projects including his UT CS website and Unity games help distinguish him from his peers. Albin is currently pursuing a degree in Computer Science in hopes of working in business management or game design.</p>
+                <p>shrestha_albin@yahoo.com</p>
+                <p>
+                    <button className="contact"><a href="tel:1-817-713-6090">Contact</a></button>
+                </p>
+            </div>
+        </div>
     </div>
 )
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,6 @@ import './App.css';
 export const Home = () => (
 
         <div className="Home text-center">
-            
             <h1>Welcome!</h1>
         </div>
 )

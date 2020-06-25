6221f9380d46067b0cee95594e41e7c921a8b3b3
Albin Shrestha <shrestha_albin@yahoo.com>
2020-06-25 16:14:00 -0500

    more updates to about page

--- src/About.js
+++ src/About.js
@@ -18,12 +18,17 @@ export const About = () => (
                 <div class="pic">
                     <img src={albin} alt="Logo"/>
                 </div>
+                <div><br/></div>
                 <p className="role">Frontend</p>
                 <p>Albin Shrestha connects tech and project management companies with the information and resources they need in order to properly train their employees. At the University of Texas, Albin has learned how to appropriately communicate with peers and co-workers creating a productive workflow. His personal projects including his UT CS website and Unity games help distinguish him from his peers. Albin is currently pursuing a degree in Computer Science in hopes of working in business management or game design.</p>
                 <p>shrestha_albin@yahoo.com</p>
+                <p>Number of commits: </p>
+                <p>Number of issues: </p>
+                <p>Number of Unit tests: </p>
                 <p>
                     <button className="contact"><a href="tel:1-817-713-6090">Contact</a></button>
                 </p>
+                <div><br/></div>
                 <div><br/></div>
             </div>
 
@@ -33,9 +38,13 @@ export const About = () => (
                 <div className="pic">
                     <img src={yijing} alt="Me"/>
                 </div>
+                <div><br/></div>
                 <p className="role">Frontend</p>
-                <p>I have 3 cats.</p>
+                <p>I have 3 cats. I am a college student in the Department of Computer Science at the University of Texas at Austin. I am interested in web design.</p>
                 <p>a1024507613@gmail.com</p>
+                <p>Number of commits: </p>
+                <p>Number of issues: </p>
+                <p>Number of Unit tests: </p>
                 <p>
                     <button className="contact"><a href="www.cs.utexas.edu/~yijing">Contact</a></button>
                 </p>
@@ -48,10 +57,14 @@ export const About = () => (
                 <div className="pic">
                     <img src={noah} alt="Me"/>
                 </div>
+                <div><br/></div>
                 <p className="role">Backend</p>
                 <p>I'm Noah Lindley, a Computer Science student at The University of Texas at Austin.
                     I'm interested in the fields of Fin-Tech and Data Science. I enjoy problem-solving and working in team based environments where I can improve my personal and technical skills. In my spare time I enjoy running, making music, writing code, watching a plethora of sports (Used to) and experiencing the fun Austin has to offer such as drinking new local brews.</p>
                 <p>lindleywnoah@gmail.com</p>
+                <p>Number of commits: </p>
+                <p>Number of issues: </p>
+                <p>Number of Unit tests: </p>
                 <p>
                     <button className="contact"><a href="mailto: lindleywnoah@gmail.com">Contact</a></button>
                 </p>
@@ -64,9 +77,13 @@ export const About = () => (
                 <div className="pic">
                     <img src={kevin} alt="Me"/>
                 </div>
+                <div><br/></div>
                 <p className="role">Backend</p>
                 <p>I am a third year computer science student at UT Austin. I am interested in machine learning.</p>
                 <p>luluwen2000@gmail.com</p>
+                <p>Number of commits: </p>
+                <p>Number of issues: </p>
+                <p>Number of Unit tests: </p>
                 <p>
                     <button className="contact"><a href="mainto: a1024507613@gmail.com">Contact</a></button>
                 </p>
@@ -79,12 +96,17 @@ export const About = () => (
                 <div className="pic">
                     <img src={li} alt="Me"/>
                 </div>
+                <div><br/></div>
                 <p className="role">Database</p>
                 <p>I am a UTCS senior student. Having interests in programming, I transfered to CS in my junior year. I am trying to catch up with my peers, honing my skills to become a professional programmer.</p>
                 <p>xiaofeili@utexas.edu</p>
+                <p>Number of commits: </p>
+                <p>Number of issues: </p>
+                <p>Number of Unit tests: </p>
                 <p>
                     <button className="contact"><a href="mainto: xiaofeili@utexas.edu">Contact</a></button>
                 </p>
+                <div><br/></div>
                 <div><br/></div>
             </div>
 

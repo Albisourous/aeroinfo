997e9780d151476f5c09ef9a8726ab94c3911d35
Yijing <a1024507613@gmail.com>
2020-06-26 16:33:31 -0700

    get IDB1.log file

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('place html file here.html')
-
-@app.route('/aircrafts')
-def aircrafts():
-    return render_template('place aircraft html file here')
+    return render_template('Home.js')
 
 @app.route('/airplanes')
 def airplanes():
-    return render_template('html file')
+    return render_template('Airplanes.js')
+
+@app.route('/flights')
+def flights():
+    return render_template('Flights.js')
 
 @app.route('/airports')
 def airports():
-    return render_template('html file')
+    return render_template('Airports.js')
 
 if __name__ == "__main__":
     # might pass host = url_address in app.run. like app.run(host = 'url_address')

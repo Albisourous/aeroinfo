990a54b5a7d26aefb9b4d6bf740d7423b0b4391c
Albin Shrestha <shrestha_albin@yahoo.com>
2020-07-08 09:29:47 -0500

    updated sort features on cards

--- front-end/src/Airports.js
+++ front-end/src/Airports.js
@@ -1,28 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import data from "./Data/airports.json";
 import InfoGrid from "./Component/InfoGrid";
 import INFO_TYPES from './Constants/enum';
+import './setup.css';
 
+let order = 1;
+const airportsData = data["data"];
+
+function GetSortOrder(prop, ord) {
+    return function (a, b) {
+        if (ord === 1) {
+            if (a[prop] > b[prop]) {
+                return 1;
+            } else if (a[prop] < b[prop]) {
+                return -1;
+            }
+            return 0;
+        } else {
+            if (a[prop] < b[prop]) {
+                return 1;
+            } else if (a[prop] > b[prop]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
 
 const Airports = props => {
-  const airportsData = data["data"];
-  return (
-    <div>
-        <li>
-            <button>Date</button>
-            <button>Departure</button>
-            <button>Arrival</button>
-            <button className="disabled">Sort:</button>
-        </li>
-        <div><br/></div>
-        <InfoGrid infoData={airportsData} infoCardType={INFO_TYPES.AIRPORTS}/>
-    </div>
-  );
+    const [m, setM] = useState(airportsData);
+    return (
+        <div>
+            <div className="sort">
+                <div className="row justify-content-end">
+                    <i className="fa fa-sort-alpha-asc" aria-hidden="true"></i>
+
+                    <button type="button" className="btn btn-outline-light" onClick={() => {
+                        let newM = [...m];
+                        newM.sort(GetSortOrder("airport_name", order));
+                        order = order * -1;
+                        setM(newM);
+                    }}>Name
+                    </button>
+
+                    <button type="button" className="btn btn-outline-light" onClick={() => {
+                        let newM = [...m];
+                        newM.sort(GetSortOrder("country_name", order));
+                        order = order * -1;
+                        setM(newM);
+                    }}>Country of Origin
+                    </button>
+                </div>
+            </div>
+            <div><br/></div>
+            <InfoGrid infoData={m} infoCardType={INFO_TYPES.AIRPORTS}/>
+        </div>
+    );
 };
 
-Airports.propTypes = {
-  
-};
+Airports.propTypes = {};
 
 export default Airports;

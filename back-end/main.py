9c90c54d3d3f9e1dee95726a76a5186ba163055a
Xiaofei Li <xiaofeili@utexas.edu>
2020-07-08 03:08:52 -0500

    api

--- back-end/main.py
+++ back-end/main.py
@@ -12,28 +12,56 @@ from flask import Flask
 import json
 import sys
 from sqlalchemy import create_engine
-import random
 import os
 from flask import jsonify
-from models import app, db, Airplane, Airport, Flight
+from models import app, db, Airplane, Airport, Flight, airport_schema, flight_schema, airplane_schema
 from flightsapi import get_all_flights
 from airportsapi import get_all_airports
 from airplanesapi import get_all_airplanes
+from flask_cors import CORS, cross_origin
 
-@app.route('/')
+
+@app.route('/api/', methods = ["GET"])
+@cross_origin()
 def index():
 	return 'hello'
 
-@app.route('/airport')    
-def airport():
-    airport_list = db.session.query(Airport).all()
-    l = list()
-    for i in airport_list:
-        l.append(i.airport_name)
-    return jsonify(l)
+@app.route('/api/airports', methods = ["GET"]) 
+@cross_origin() 
+def airports():
+    
+    return getAirports()
+    
+@app.route('/api/airplanes', methods = ["GET"])
+@cross_origin()  
+def airplanes():
+
+    return getFlights()
+    
+@app.route('/api/flights', methods = ["GET"]) 
+@cross_origin()  
+def flights():
+
+    return getAirplanes()
+    
+def getAirports():
+    airports = db.session.query(Airport).all()
+    result = airport_schema.dump(airports)
+    return jsonify({'airports': result})
+        
+def getFlights():
+    flights = db.session.query(Flight).all()
+    result = flight_schema.dump(flights)
+    return jsonify({'flights': result})
+
+def getAirplanes():
+    airplanes = db.session.query(Airplane).all()
+    result = airplane_schema.dump(airplanes)
+    return jsonify({'airplanes': result})
+        
     
     
 if __name__ == "__main__":
-	app.run(host='127.0.0.1', port=8080)
+	app.run(host='127.0.0.1', port = 8080)
 
 

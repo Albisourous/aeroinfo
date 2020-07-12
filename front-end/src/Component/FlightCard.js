fef778542ba35821a8ca49491a305c8c5e8c8a0f
Yijing <yijing@YijingdeMacBook-Pro.local>
2020-07-11 19:18:18 -0700

    fix card size

--- front-end/src/Component/FlightCard.js
+++ front-end/src/Component/FlightCard.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import ButtonBase from "@material-ui/core/ButtonBase";
-import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
     root: {
-        width: 345,
+        width: 317,
         height: 225,
     },
     
